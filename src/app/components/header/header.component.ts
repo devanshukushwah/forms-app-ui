4cacfe9246b5733e2bb3b11023d6e15458146c53
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-02-02 00:36:03 +0530

    add logout button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToolbarModule } from 'primeng/toolbar';
 import { NavigateService } from '../../core/navigate.service';
 import { LogoComponent } from '../logo/logo.component';
+import { KeycloakService } from '../../services/keycloak.service';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ToolbarModule, LogoComponent],
+  imports: [ToolbarModule, LogoComponent, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  constructor(public navigateService: NavigateService) {}
+export class HeaderComponent implements OnInit {
+  firstLastname: string = '';
+
+  constructor(
+    public navigateService: NavigateService,
+    public keyCloakService: KeycloakService
+  ) {}
+
+  async ngOnInit() {
+    const user = await this.keyCloakService.loadUserProfile();
+    if (user) {
+      this.firstLastname = user?.firstName + ' ' + user?.lastName;
+    }
+  }
 }
