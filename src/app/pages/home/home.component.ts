ac05fe8f1ce24de2b961213a0a743a1ddaa57c45
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-02-04 00:13:07 +0530

    add home page changes.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { CardModule } from 'primeng/card';
 import { TagModule } from 'primeng/tag';
 import { DividerModule } from 'primeng/divider';
 import { PricingComponent } from '../../components/pricing/pricing.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -16,10 +17,57 @@ import { PricingComponent } from '../../components/pricing/pricing.component';
     ButtonModule,
     ClientsLogosComponent,
     PricingComponent,
+    CardModule,
+    CommonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  pricingCards = [
+    {
+      title: 'Free Plan',
+      price: {
+        amount: '₹0',
+        period: 'per month',
+        tag: 'Demo',
+      },
+      features: [
+        '10 Form Creations',
+        'No Analytics',
+        'No Support',
+        'No Branding',
+      ],
+    },
+    {
+      title: 'Basic Plan',
+      price: {
+        amount: '₹499',
+        period: 'per month',
+        tag: 'Best Value',
+      },
+      features: [
+        '1000 Form Creations',
+        'Passive Analytics',
+        'Shared Support',
+        'No Branding',
+      ],
+    },
+    {
+      title: 'Pro Plan',
+      price: {
+        amount: '₹4999',
+        period: 'per month',
+        tag: 'Most Popular',
+      },
+      features: [
+        'Unlimited Form Creations',
+        'Real-time Analytics',
+        'Priority Support',
+        'Custom Branding',
+      ],
+    },
+  ];
+
   constructor(public navigateService: NavigateService) {}
 }
